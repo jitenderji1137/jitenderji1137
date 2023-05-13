5ec69de5a7eebfb5be4fa6459599dcbf4c68d2d7
jitender <trademetrader1137@gmail.com>
2023-05-13 17:01:56 +0530

    working on it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Nav from "./Nav";
 import Left from "./Left";
 import Right from "./Right";
+import Main from "./Main";
 export default function App() {
     return(
       <>
       <Nav/>
       <Left/>
       <Right/>
+      <Main/>
       </>
     );
 }
---
+++ src/Main.jsx
@@ -0,0 +1,23 @@
+import "./Main.css";
+export default function Main(){
+    return(
+        <>
+        <div>
+            <main className="main">
+                <section className="MyName">
+                    <h1>Hello, my name is</h1>
+                    <h2>Jitender.</h2>
+                    <h3>I love exploring new things!</h3>
+                    <div className="paragraph">
+                        <p>As a Masai School graduate, I possess a unique set of skills and knowledge that enable me to innovative new idea's. I have developed a strong foundation in programming languages and technologies, allowing me to review high-quality products.</p>
+                        <p>I enjoy learning new skills and implementing them in real life!</p>
+                    </div>
+                    <div className="Getintouch">
+                        <a href="mailto:trademetrader1137@gmail.com">Get In Touch</a>
+                    </div>
+                </section>
+            </main>
+        </div>
+        </>
+    );
+}
