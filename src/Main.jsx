2c7e887d4d0fb4b3ce367ec1060193a14964ea7d
jitender <trademetrader1137@gmail.com>
2023-05-13 22:13:00 +0530

    working on it

--- src/Main.jsx
+++ src/Main.jsx
@@ -18,10 +18,37 @@ export default function Main(){
                 </section>
                 <section className="Aboutme">
                     <h3>About Me</h3>
-                    <div>
-                        <div></div>
-                        <div></div>
+                    <div className="AboutPara">
+                        <div className="AboutPara1">
+                            <div>
+                                <p>Hello! I'm Jitender, from Haryana, is known for its agricultural and industrial production.</p>
+                                <p>I am a creative, time punctual, dedicated, goal-oriented individual with decent moral Values and Ethicates along with a high-energy level, honed communication skills, strong organizational skills, and meticulous attention to detail.</p>
+                                <p></p>
+                                <p>I have successfully achieved 70% in 12th standard(2021-2022), paving the way for a bright future.</p>
+                                <p>Here are a few of the other activities that I love to do!</p>
+                            </div>
+                            <ul>
+                                <li>Coding</li>
+                                <li>Problem Solving</li>
+                                <li>Team Management</li>
+                                <li>Discipline</li>
+                                <li>Crafting</li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                        <div className="AboutPara2">
+                            <a href="https://github.com/jitenderji1137">
+                                <div>
+                                    <img src="https://github.com/jitenderji1137/nested-loop/assets/113350806/6d0a36f0-a468-45c8-ba4c-61f8c886dcd0" alt=""></img>
+                                </div>
+                            </a>
+                        </div>
                     </div>
+                </section>
+                <section className="Experence">
+                    <h3>My Journy</h3>
+                    <div></div>
                 </section>
             </main>
         </div>
