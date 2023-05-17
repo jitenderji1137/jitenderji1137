2660c4ebc07e6a87025b1aa6b376ed53ef120433
jitender <trademetrader1137@gmail.com>
2023-05-17 22:49:41 +0530

    Working on it

--- src/Main.jsx
+++ src/Main.jsx
@@ -9,7 +9,7 @@ export default function Main(){
                     <h2>Jitender.</h2>
                     <h3>I love exploring new things!</h3>
                     <div className="paragraph">
-                        <p>As a Masai School graduate, I possess a unique set of skills and knowledge that enable me to innovative new idea's. I have developed a strong foundation in programming languages and technologies, allowing me to review high-quality products.</p>
+                        <p>As a <a href="https://masaischool.com/">Masai School</a> graduate, I possess a unique set of skills and knowledge that enable me to innovate new idea's. I have developed a strong foundation in programming languages and technologies, allowing me to review high-quality products.</p>
                         <p>I enjoy learning new skills and implementing them in real life!</p>
                     </div>
                     <div className="Getintouch">
@@ -25,7 +25,7 @@ export default function Main(){
                                 <p>I am a creative, time punctual, dedicated, goal-oriented individual with decent moral Values and Ethicates along with a high-energy level, honed communication skills, strong organizational skills, and meticulous attention to detail.</p>
                                 <p></p>
                                 <p>I have successfully achieved <span>70%</span> in <span>12th</span> standard(<span>2021-2022</span>), paving the way for a <span>bright future</span>.</p>
-                                <p>Here are a few of the activities that <span>I love to do</span>!</p>
+                                <p>Here are few of the activities that <span>I love to do</span>!</p>
                             </div>
                             <ul>
                                 <li>Coding</li>
